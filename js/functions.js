72b6506d105fb32190490bb77530c5629ce5a2ee
Callum Chisholm <callum@techgorilla.io>
2018-05-15 17:11:26 +1000

    'update'

--- js/functions.js
+++ js/functions.js
@@ -1,20 +1,12 @@
 (function($) {
     //Old creative
     "use strict"; // Start of use strict
-    var navbarCollapse = function () {
-        var header = $('#header');
-        if (header.length) {
-            var scroll = header.offset().top;
-            console.log(header);
-            if(scroll < 2) {
-                $("#header").addClass("sticky-header");
-            }else{
-                $("#header").removeClass("sticky-header");
-            }
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        //>=, not <=
+        if (scroll >= 2) {
+            //clearHeader, not clearheader - caps H
+            $("#header").addClass("sticky-header");
         }
-    };
-    // Collapse now if page is not at top
-    navbarCollapse();
-    // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
+    });
 })(jQuery); // End of use strict
